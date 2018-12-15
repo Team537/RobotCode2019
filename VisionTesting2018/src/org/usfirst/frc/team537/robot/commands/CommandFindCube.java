b310216245a9f238a8169b698908fc2e0114be82
Aaron Phalphouvong <aaronphal@gmail.com>
2018-12-15 12:04:02 -0600

    maths

--- VisionTesting2018/src/org/usfirst/frc/team537/robot/commands/CommandFindCube.java
+++ VisionTesting2018/src/org/usfirst/frc/team537/robot/commands/CommandFindCube.java
@@ -33,6 +33,15 @@ public class CommandFindCube extends Command {
 	public double alignRate;
 	public static double rate;
 	
+	private int WIDTH = 320;
+	private int screenPercent;
+	public double widthInches;
+	public static double distance;
+	public double scaleFactor = 1;
+	public static double angle;
+	private static CommandDriveRate driveTo;
+	
+	
     public CommandFindCube() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
@@ -42,18 +51,35 @@ public class CommandFindCube extends Command {
     		if (!pipeline.filterContoursOutput().isEmpty()) {
     			r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
     			synchronized (imgLock) {
-    				/*
-    				centerX = r.x + (r.width / 2);
-    				rate = (centerX - (RobotMap.Vision.IMG_WIDTH / 2)) / 2;
-    				*/
+    				
+    				
+    				
+    				//centerX = r.x + (r.width / 2);
+    				//rate = (centerX - (RobotMap.Vision.IMG_WIDTH));
+    				
+    				
     				
     				double halfWidth = (double) r.width / 2.0;
-    				rate = (((double) r.x - halfWidth) / halfWidth) / 2;
-    				if(rate > 0.60) {
+    				//rate = (((double) r.x - halfWidth) / halfWidth);
+    				
+    				double pos = ((double) r.x - halfWidth);
+    				angle = (pos - (WIDTH/2)) * 0.217;
+    				
+    				/*
+    				if(rate < 40) {
     					targetFound = true;
     				} else {
     					targetFound = false;
     				}
+    				
+    				double xPos = r.x + (r.width / 2);
+    				angle = Math.atan(xPos/12);
+    				
+    				*/
+    				
+    				
+    				
+    				
     			} 
     		} else {
 				rate = 0.250;
@@ -74,7 +100,11 @@ public class CommandFindCube extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.subsystemDrive.setTarget(0.00, rate, 0.0f, 0.0f);
+    	
+    	Robot.subsystemDrive.setTarget(Robot.subsystemGyro.getAngle(), angle, 0.0f);
+    	//Robot.subsystemDrive.setTarget(Robot.subsystemGyro.getAngle(), 0.0, 0.0, 0.0);
+    	//driveTo = new CommandDriveRate(angle, 0.0 , 0.10);
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -92,5 +122,6 @@ public class CommandFindCube extends Command {
     // subsystems is scheduled to run
     protected void interrupted() {
     	Robot.subsystemDrive.stop();
+    	//Robot.subsystemDrive.setTarget(Robot.subsystemGyro.getAngle(), 0.0, 0.0);
     }
 }
