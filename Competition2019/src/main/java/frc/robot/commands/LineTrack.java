f27e0fa9d54d79d912ca752896f8bf2d17d0c041
Aaron Phalphouvong <aaronphal@gmail.com>
2019-01-18 11:29:49 -0600

    Removed Comments

--- Competition2019/src/main/java/frc/robot/commands/LineTrack.java
+++ Competition2019/src/main/java/frc/robot/commands/LineTrack.java
@@ -18,19 +18,15 @@ public class LineTrack extends Command {
     
   }
 
-  // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
     return false;
   }
 
-  // Called once after isFinished returns true
   @Override
   protected void end() {
   }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
   @Override
   protected void interrupted() {
   }
