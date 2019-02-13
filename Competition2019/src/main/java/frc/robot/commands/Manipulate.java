e1cf1c8d31859ec5caad71db351e1b7090dd9b0c
Alex71827 <44625347+Alex71827@users.noreply.github.com>
2019-02-12 18:59:41 -0600

    Update Manipulate.java

--- Competition2019/src/main/java/frc/robot/commands/Manipulate.java
+++ Competition2019/src/main/java/frc/robot/commands/Manipulate.java
@@ -1,53 +1,35 @@
 package frc.robot.commands;
 
+import static frc.robot.Robot.m_manipulator;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Maths;
 import frc.robot.Robot;
-import frc.robot.RobotMap;
-import frc.robot.joysticks.JoystickF310;
-import static frc.robot.Robot.m_manipulator;
 
 public class Manipulate extends Command {
 
-    public static boolean UP = true;
-    public static boolean DOWN = false;
+    public static int setting = 0;
 
-    public boolean finished = false;
-    public boolean dir;
+    public long lastMillis;
 
-    public Manipulate(boolean dir) { // possible dir error
+    public Manipulate() { // possible dir error
         requires(Robot.m_manipulator);
-        this.dir = dir;
-    }
-
-    public Manipulate() {
-        
+        lastMillis = System.currentTimeMillis();
     }
 
     public static double calcFromRaw(double rawTenBit) {
         return rawTenBit / 1023D;
     }
 
-    public static int check(double rawRatio) {
-        if (isInRange(rawRatio, getRange(m_manipulator.currSetting)))
-            return 0;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                
-            }
-        });
-        return 0;
-    }
-
-    public static double[] getRange(int setting) {
+    public static double[] getRange() {
         if (setting == 0)
-            return new double[] {0D, 0.02D};
+            return new double[] {0D, 0.01D};
         else if (setting == 1)
             return new double[] {0.49D, 0.51D};
         else
-            return new double[] {0.98D, 1D};
+            return new double[] {0.99D, 1D};
     }
 
     public static boolean isInRange(double val, double[] range) {
@@ -62,11 +44,42 @@ public class Manipulate extends Command {
         return false;
     }
 
+    public static double dstFrmRange(double val, double[] range) {
+        try {
+            double min = range[0];
+            double max = range[1];
+            if (val < min)
+                return min - val;
+            else if (val > max) {
+                return val - max;
+            }
+        } catch (Exception e) {
+
+        }
+        return 0;
+    }
+
     @Override
     protected void execute() {
-        double d = calcFromRaw(m_manipulator.wrist.getSensorCollection().getAnalogInRaw());
-        System.out.println(d);
-        if (m_manipulator.armTriggerRight.getRawButton(6))
+        {
+            double d = calcFromRaw(m_manipulator.wrist.getSensorCollection().getAnalogInRaw());
+            System.out.println("Wrist: " + d);
+            if (!isInRange(d, getRange())) {
+                m_manipulator.wrist.set(ControlMode.PercentOutput, 0D);
+            } else {
+                m_manipulator.wrist.set(ControlMode.PercentOutput, Maths.clamp(dstFrmRange(d, getRange()), 0.05D, 1D));
+            }
+        }
+        {
+            double d = calcFromRaw(m_manipulator.shoulder.getSensorCollection().getAnalogInRaw());
+            System.out.println("Shoulder: " + d);
+            if (!isInRange(d, getRange())) {
+                m_manipulator.shoulder.set(ControlMode.PercentOutput, 0D);
+            } else {
+                m_manipulator.shoulder.set(ControlMode.PercentOutput, Maths.clamp(dstFrmRange(d, getRange()), 0.05D, 1D));
+            }
+        }
+        /*if (m_manipulator.armTriggerRight.getRawButton(6))
             m_manipulator.armTriggerRight.set(ControlMode.PercentOutput, JoystickF310.Keys.BUMPER_RIGHT * RobotMap.ROBOT.MANIPULATOR_SPEED); //manipulator
         if (m_manipulator.armTriggerLeft.getRawButton(5))
             m_manipulator.armTriggerLeft.set(ControlMode.PercentOutput, JoystickF310.Keys.BUMPER_LEFT * -RobotMap.ROBOT.MANIPULATOR_SPEED); //manipulator
@@ -74,14 +87,13 @@ public class Manipulate extends Command {
             m_manipulator.wristTriggerRight.set(ControlMode.PercentOutput, JoystickF310.Axis.RIGHT_TRIGGER * RobotMap.ROBOT.MANIPULATOR_SPEED); // wrist
         if (m_manipulator.wristTriggerLeft.getRawAxis(2) > 0)
             m_manipulator.wristTriggerLeft.set(ControlMode.PercentOutput, JoystickF310.Axis.LEFT_TRIGGER * -RobotMap.ROBOT.MANIPULATOR_SPEED); // wrist
-        
+        */
     }
 
     @Override
     protected boolean isFinished() {
-        return finished;
+        return false;
     }
-
     
-    }
+}
 
