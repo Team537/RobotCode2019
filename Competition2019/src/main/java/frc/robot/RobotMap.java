f820c2e7b41836dadda27ef1005e963922cfb472
Aaron Phalphouvong <aaronphal@gmail.com>
2019-03-04 06:42:24 -0600

    arm and deadband

--- Competition2019/src/main/java/frc/robot/RobotMap.java
+++ Competition2019/src/main/java/frc/robot/RobotMap.java
@@ -32,7 +32,7 @@ public class RobotMap {
 		public static final double RATIO = Math.sqrt((DEPTH * DEPTH) + (WIDTH * WIDTH));
 
 		public static final double DRIVE_M_TO_ENCODER = 1984.4878; // ticks/m
-		public static final double DRIVE_SPEED = 0.80; // % 0-1.0
+		public static final double DRIVE_SPEED = 0.80; // % 0-100
 		
 		public static final double DRIVE_PATH_SCALE = 0.4; // % -1.0-1.0 speed scale for the pathfinder
 
--- Competition2019/src/main/java/frc/robot/commands/DriveArcade.java
+++ Competition2019/src/main/java/frc/robot/commands/DriveArcade.java
@@ -24,12 +24,24 @@ public class DriveArcade extends Command {
   protected void execute() {
     
     double gyro = Math.toRadians(Robot.m_gyro.getAngle());
-    double rotation = Robot.m_oi.m_main.getRawAxis("DriveRotation");
-		double strafe = Robot.m_oi.m_main.getRawAxis("DriveStrafe");
-    double forward = Robot.m_oi.m_main.getRawAxis("DriveForward");
-    
+    //double rotation = Robot.m_oi.m_main.getRawAxis("DriveRotation");
+		//double strafe = Robot.m_oi.m_main.getRawAxis("DriveStrafe");
+    //double forward = Robot.m_oi.m_main.getRawAxis("DriveForward");
+  
+    double rotation = deadband(0.15, Robot.m_oi.m_main.getRawAxis("DriveRotation"));
+		double strafe = deadband(0.1, Robot.m_oi.m_main.getRawAxis("DriveStrafe"));
+    double forward = deadband(0.1, Robot.m_oi.m_main.getRawAxis("DriveForward"));
     
     Robot.m_drivetrain.setTarget(0.00, rotation, strafe, forward);
+  }
+
+  private double deadband(double deadband, double input) {
+    if(input < deadband || input > -deadband) {
+      return 0.00;
+    } else {
+      double value = input - deadband;
+      return value;
+    }
   }
 
   @Override
